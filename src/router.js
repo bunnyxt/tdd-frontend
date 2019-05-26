75bee2877270dea984cedb4ead876d3bd89ca2cc
bunnyxt <bunnyxt@outlook.com>
2019-05-26 20:54:42 +0800

    Add tool category.

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ const SprintHome = () => import("./components/SprintHome");
 const SprintVideoDetail = () =>import("./components/SprintVideoDetail");
 const SprintDailyHome = () => import("./components/SprintDailyHome");
 const SprintDailyDetail = () => import("./components/SprintDailyDetail");
+const ToolHome = () => import("./components/tool/ToolHome");
+const DateCalc = () => import("./components/tool/DateCalc");
 const NotFound = () => import("./components/NotFound");
 
 Vue.use(Router);
@@ -32,6 +34,12 @@ export default new Router({
         }, {
             path: "/sprint/daily/:date",
             component: SprintDailyDetail
+        }, {
+            path: "/tool",
+            component: ToolHome
+        }, {
+            path: "/tool/datecalc",
+            component: DateCalc
         }, {
             path: "*",
             component: NotFound
