f8461ad1a005565a3a2ce828651be6c13e8237ef
bunnyxt <bunnyxt@outlook.com>
2019-06-09 16:02:07 +0800

    Manual mark special point in sprint daily chart.

--- src/components/SprintDailyChart.vue
+++ src/components/SprintDailyChart.vue
@@ -17,6 +17,261 @@ export default {
         }
       ],
       chartOptions: {
+        annotations: {
+          points: [{
+            x: new Date('2019-03-20').valueOf(),
+            y: 15389,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+大氿歌',
+            }
+          },{
+            x: new Date('2019-03-25').valueOf(),
+            y: 35735,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+大时代',
+            }
+          },{
+            x: new Date('2019-03-26').valueOf(),
+            y: 22812,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+夜间出租车',
+            }
+          },{
+            x: new Date('2019-03-31').valueOf(),
+            y: 12152,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'lightgreen',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#00E396',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: '-大氿歌',
+            }
+          },{
+            x: new Date('2019-04-01').valueOf(),
+            y: 22901,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+得过且过的勇者',
+            }
+          },{
+            x: new Date('2019-04-24').valueOf(),
+            y: 7086,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'lightgreen',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#00E396',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: '-得过且过的勇者',
+            }
+          },{
+            x: new Date('2019-04-26').valueOf(),
+            y: 6858,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+最美的夏天',
+            }
+          },{
+            x: new Date('2019-04-28').valueOf(),
+            y: 18155,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'lightgreen',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#00E396',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: '-达拉崩吧（王浩然）',
+            }
+          },{
+            x: new Date('2019-04-29').valueOf(),
+            y: 9998,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+多情岸',
+            }
+          },{
+            x: new Date('2019-05-01').valueOf(),
+            y: 93147,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+售楼小姐',
+            }
+          },{
+            x: new Date('2019-04-26').valueOf(),
+            y: 6858,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+最美的夏天',
+            }
+          },{
+            x: new Date('2019-05-04').valueOf(),
+            y: 15060,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'lightgreen',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#00E396',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: '-售楼小姐',
+            }
+          },{
+            x: new Date('2019-05-26').valueOf(),
+            y: 9654,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'lightgreen',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#00E396',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#00E396',
+              },
+              text: '-星愿',
+            }
+          },{
+            x: new Date('2019-06-05').valueOf(),
+            y: 6858,
+            marker: {
+              size: 4,
+              fillColor: '#fff',
+              strokeColor: 'red',
+              radius: 2,
+            },
+            label: {
+              borderColor: '#FF4560',
+              offsetY: 0,
+              style: {
+                color: '#fff',
+                background: '#FF4560',
+              },
+              text: '+非人哉',
+            }
+          },]
+        },
         chart: {
           toolbar: {
             tools: {
@@ -80,7 +335,7 @@ export default {
   },
   methods: {
     dailyDateToTs: function(dailyDate) {
-      var date = new Date(dailyDate.substring(0, 4)+'/'+dailyDate.substring(4, 6)+'/'+dailyDate.substring(6, 8))
+      var date = new Date(dailyDate.substring(0, 4)+'-'+dailyDate.substring(4, 6)+'-'+dailyDate.substring(6, 8))
       return date.valueOf()
     }
   }
