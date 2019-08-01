506f9d3e0330924ff524f4d1244ad40f1a8dbb2d
bunnyxt <bunnyxt@outlook.com>
2019-08-01 13:16:25 -0700

    Fix bug which cause wrong display of data in different time zone.

--- src/components/SprintVideoHeatMapChart.vue
+++ src/components/SprintVideoHeatMapChart.vue
@@ -15,7 +15,56 @@ export default {
   data: function() {
     return {
       weekIndex: 1, // from 1 to length
-      seriesList: [],
+      seriesList: [
+        {
+          name: "22:00 - 24:00",
+          data: []
+        },
+        {
+          name: "20:00 - 22:00",
+          data: []
+        },
+        {
+          name: "18:00 - 20:00",
+          data: []
+        },
+        {
+          name: "16:00 - 18:00",
+          data: []
+        },
+        {
+          name: "14:00 - 16:00",
+          data: []
+        },
+        {
+          name: "12:00 - 14:00",
+          data: []
+        },
+        {
+          name: "10:00 - 12:00",
+          data: []
+        },
+        {
+          name: "08:00 - 10:00",
+          data: []
+        },
+        {
+          name: "06:00 - 08:00",
+          data: []
+        },
+        {
+          name: "04:00 - 06:00",
+          data: []
+        },
+        {
+          name: "02:00 - 04:00",
+          data: []
+        },
+        {
+          name: "00:00 - 02:00",
+          data: []
+        },
+      ],
       chartOptions: {
         chart: {
           toolbar: {
@@ -39,14 +88,16 @@ export default {
       let startTs = records[0].added
       // var endTs = records[records.length - 1].added
 
-      let startWeekTs = startTs - (startTs % (60 * 60 * 24 * 7)) // modify week to Monday
-      if (startTs - startWeekTs >= (60 * 60 * 24 * 4)) {
-        startWeekTs += (60 * 60 * 24 * 4)
-      } else {
-        startWeekTs -= (60 * 60 * 24 * 3)
-      }
-      startWeekTs -= (60 * 60 * 8) // UTC+8
-      
+      // calc start week ts
+      let startDate = new Date(startTs * 1000)
+      let startDateDay = startDate.getDay()
+      let startDateHours = startDate.getHours()
+      let startDateMinutes = startDate.getMinutes()
+      let startDateSeconds = startDate.getSeconds()
+      let startWeekTs = startTs - (startDateHours * 60 * 60) - (startDateMinutes * 60) - (startDateSeconds)
+      startDateDay = (startDateDay + 6) % 7
+      startWeekTs -= (startDateDay *  24 * 60 * 60)
+
       let p1 = 0
       let p2 = 1
 
@@ -65,18 +116,20 @@ export default {
         while (p2 < records.length) {
           let ts1 = records[p1].added
           let v1 = records[p1].view
-          let w1 = (new Date(ts1 * 1000).getDay() + 6) % 7
-          let h1 = parseInt(new Date(ts1 * 1000).getHours() / 2)
+          let d1 = new Date(ts1 * 1000)
+          let w1 = (d1.getDay() + 6) % 7
+          let h1 = Math.floor(d1.getHours() / 2)
 
           let ts2 = records[p2].added
           let v2 = records[p2].view
-          let w2 = (new Date(ts2 * 1000).getDay() + 6) % 7
-          let h2 = parseInt(new Date(ts2 * 1000).getHours() / 2)
+          let d2 = new Date(ts2 * 1000)
+          let w2 = (d2.getDay() + 6) % 7
+          let h2 = Math.floor(d2.getHours() / 2)
 
           if (ts2 >= endWeekTs) {
             // last record
             let v11 = (v2 - v1) / (ts2 - ts1) * (endWeekTs - ts1)
-            matrix[w1][h1] += parseInt(v11)
+            matrix[w1][h1] += Math.floor(v11)
             break;
           }
 
@@ -85,15 +138,18 @@ export default {
             matrix[w2][h2] += (v2 - v1)
           } else if ((w2 * 12 + h2) - (w1 * 12 + h1) == 1){
             // cross block
-            let tss = ts2 - (ts2 % 7200)
+            let d2_h = d2.getHours()
+            let d2_m = d2.getMinutes()
+            let d2_s = d2.getSeconds()
+            let tss = ts2 - (d2_h % 2) * (60 * 60) - d2_m * 60 - d2_s // mid separation ts
             let v11 = (v2 - v1) / (ts2 - ts1) * (tss - ts1)
             let v22 = (v2 - v1) / (ts2 - ts1) * (ts2 - tss)
-            matrix[w1][h1] += parseInt(v11)
-            matrix[w2][h2] += parseInt(v22)
+            matrix[w1][h1] += Math.floor(v11)
+            matrix[w2][h2] += Math.floor(v22)
           } else if (ts1 < startWeekTs) {
             // first record
             let v22 = (v2 - v1) / (ts2 - ts1) * (ts2 - startWeekTs)
-            matrix[w2][h2] += parseInt(v22)
+            matrix[w2][h2] += Math.floor(v22)
           }
 
           p1++
@@ -174,7 +230,7 @@ export default {
         startWeekTs += (60 * 60 * 24 * 7) // go to next week
       }
 
-      return seriesList    
+      return seriesList
     },
    formatDate: function(ts) {
       let date = new Date(ts * 1000)
