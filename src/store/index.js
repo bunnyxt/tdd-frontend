c84ed20365bea2018e4201fdc514e29e78be6229
bunnyxt <bunnyxt@outlook.com>
2019-08-04 15:31:18 -0700

    Update api base from http to https.

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    apiBase: "http://api.bunnyxt.com/tdd/",
+    apiBase: "https://api.bunnyxt.com/tdd/",
     //apiBase: "http://localhost/tdd/", // debug
   }
 })
