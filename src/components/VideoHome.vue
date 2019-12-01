332f2bedc542213c9c26292b3bd4b59419eb9235
bunnyxt <bunnyxt@outlook.com>
2019-12-01 14:07:43 +0800

    Add video detail drawer demo.

--- src/components/VideoHome.vue
+++ src/components/VideoHome.vue
@@ -97,8 +97,13 @@
     <div class="section-block">
       <a-spin :spinning="isLoadingVideoList">
         <a-list itemLayout="vertical" size="large" :dataSource="videoList">
-          <a-list-item slot="renderItem" slot-scope="item" key="item.id">
-            <a-row>
+          <a-list-item
+              slot="renderItem"
+              slot-scope="item"
+              key="item.id"
+          >
+            <div class="video-item" @click="videoItemClickHandler(item.id)">
+            <a-row >
               <a-col :xs="24" :sm="8" :md="6" :xl="4" style="padding: 8px">
                 <img width="100%" height="100%" alt="pic" :src="item.pic"/>
               </a-col>
@@ -115,20 +120,59 @@
                   <a-icon type="calendar" style="margin-left: 8px; margin-right: 4px"/>
                   {{ tsToStr(item.pubdate) }}
                 </p>
+<!--                <p>TODO: latest stat info</p>-->
+                <p></p>
               </a-col>
             </a-row>
+            </div>
           </a-list-item>
         </a-list>
-        <div v-if="!isLoadingVideoList">
-          <a-pagination
-              showQuickJumper
-              v-model="pagiCurrent"
-              :total="videoTotalCount"
-              :pageSize="20"
-              @change="onPagiChange"
-          />
-        </div>
+        <a-pagination
+            showQuickJumper
+            v-model="pagiCurrent"
+            :total="videoTotalCount"
+            :pageSize="20"
+            @change="onPagiChange"
+        />
       </a-spin>
+    </div>
+    <div v-if="videoDetailDrawerCurrentVideo">
+      <a-drawer
+          :title="'av' + videoDetailDrawerCurrentVideo.aid.toString()"
+          placement="right"
+          :closable="true"
+          @close="videoDetailDrawerCloseHandler"
+          :visible="videoDetailDrawerVisible"
+      >
+        <h3>{{ videoDetailDrawerCurrentVideo.title }}</h3>
+        <p>
+          <a-avatar size="small" :src="videoDetailDrawerCurrentVideo.member.face" style="margin-right:12px"/>
+          <a :href="'https://space.bilibili.com/'+videoDetailDrawerCurrentVideo.mid" target="_blank">
+            {{ videoDetailDrawerCurrentVideo.member.name }}
+          </a>
+        </p>
+        <p><a-icon type="calendar" style="margin-right: 12px"/>{{ tsToStr(videoDetailDrawerCurrentVideo.pubdate) }}</p>
+        <p><a-icon type="database" style="margin-right: 12px"/>{{ videoDetailDrawerCurrentVideo.tname }}</p>
+        <a-divider orientation="left">简介</a-divider>
+        {{ videoDetailDrawerCurrentVideo.desc }}
+        <a-divider orientation="left">标签</a-divider>
+        <a-tag
+            v-for="tag in videoDetailDrawerCurrentVideo.tags.split(';').slice(0, -1)"
+            :key="tag"
+            style="margin-bottom: 4px"
+        >
+          {{ tag }}
+        </a-tag>
+        <div class="fake-drawer-footer"></div>
+        <div class="drawer-footer">
+          <div @click="videoDetailClickHandler(videoDetailDrawerCurrentVideo.aid)">
+              <a-icon type="line-chart" title="视频详情" style="margin-right: 8px"/>视频详情
+          </div>
+          <div @click="videoViewClickHandler(videoDetailDrawerCurrentVideo.aid)">
+            <a-icon type="play-circle" title="观看视频" style="margin-right: 8px"/>观看视频
+          </div>
+        </div>
+      </a-drawer>
     </div>
   </div>
 </template>
@@ -152,11 +196,19 @@
         titleValue: '',
         memberNameValue: '',
         pagiCurrent: 1,
-        videoTotalCount: 200
+        videoTotalCount: 20,
+        videoDetailDrawerVisible: false,
+        videoDetailDrawerCurrentIndex: 0
       }
     },
     computed: {
-
+      videoDetailDrawerCurrentVideo: function() {
+        if (this.videoList[this.videoDetailDrawerCurrentIndex]) {
+          return this.videoList[this.videoDetailDrawerCurrentIndex];
+        } else {
+          return null;
+        }
+      }
     },
     methods: {
       checkParams: function() {
@@ -198,11 +250,15 @@
 
         let url = this.assembleQueryUrl();
         fetch(url)
+          .then(response => {
+            this.videoTotalCount = parseInt(response.headers.get("x-total-count"));
+            return response
+          })
           .then(response => response.json())
           .then(json => this.videoList = json)
+
           .then(() => this.lastLoadVideoListDate = new Date())
           .then(() => this.isLoadingVideoList = false);
-        // TODO update total size
       },
       addZero: function(value) {
         return value < 10 ? "0" + value : "" + value;
@@ -247,10 +303,26 @@
         }
       },
       onPagiChange: function (pagiClick) {
-        // TODO
-        console.log(pagiClick);
         this.pagiCurrent = pagiClick;
         this.fetchVideoList();
+      },
+      videoDetailDrawerCloseHandler: function() {
+        this.videoDetailDrawerVisible = false;
+      },
+      videoItemClickHandler: function(id) {
+        this.videoDetailDrawerVisible = true;
+        for (let i = 0; i < this.videoTotalCount; i++) {
+          if (this.videoList[i].id === id) {
+            this.videoDetailDrawerCurrentIndex = i;
+            break;
+          }
+        }
+      },
+      videoDetailClickHandler: function(aid) {
+        this.$router.push('/video/av'+aid);
+      },
+      videoViewClickHandler: function(aid) {
+        window.open('https://www.bilibili.com/video/av'+aid);
       }
     },
     created() {
@@ -272,5 +344,35 @@
   .filter-table-label {
     width: 80px;
     white-space: nowrap;
+  }
+  .video-item {
+    transition: all .3s;
+  }
+  .video-item:hover {
+    cursor: pointer;
+    -webkit-box-shadow: 0 2px 8px rgba(0,0,0,.09);
+    -moz-box-shadow: 0 2px 8px rgba(0,0,0,.09);
+    box-shadow: 0 2px 8px rgba(0,0,0,.09);
+    border-color: rgba(0,0,0,.09);
+  }
+  .drawer-footer {
+    position: fixed;
+    bottom: 0;
+    background: #FAFAFA;
+    width: 100%;
+    margin-left: -24px;
+    border-top: 1px solid #e8e8e8;
+    cursor: pointer;
+  }
+  .drawer-footer div {
+    float: left;
+    width: 128px;
+    height: 48px;
+    text-align: center;
+    border-left: 1px solid #e8e8e8;
+    padding-top: 14px;
+  }
+  .fake-drawer-footer {
+    height: 48px;
   }
 </style>
