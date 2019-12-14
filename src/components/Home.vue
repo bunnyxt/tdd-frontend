781fe96a1f4b76c0c8f72cebfa07862309f4ef5d
bunnyxt <bunnyxt@outlook.com>
2019-12-14 11:37:59 +0800

    Finish ugly homepage.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,19 +6,25 @@
       </a-breadcrumb>
     </div>
     <div class="section-block">
+      <h1>天钿Daily - VC数据交流与分享</h1>
+      <img src="/logo_64S.png" alt="logo" />
+      <p><small>logo by SweetFairyTale</small></p>
+      <p>天钿Daily（TianDian Daily）是一个VC相关数据交流与分享的网站。本站以推动VC相关数据交流为宗旨，基于B站API以及简单的网络爬虫，定期抓取并存储VC相关数据，选取感兴趣的纬度公开展示。</p>
+      <p>
+        QQ群：<a target="_blank" href="https://jq.qq.com/?_wv=1027&k=588s7nw">537793686</a><br/>
+        个人邮箱：<a href="mailto:bunnyxt@outlook.com">bunnyxt@outlook.com</a>
+      </p>
+      <p><router-link to="/about">了解更多</router-link></p>
+    </div>
+    <div class="section-separator"></div>
+    <div class="section-block">
       <h1>传送门</h1>
-      <ul>
-        <li>
-          <router-link to="/video">所有视频</router-link>：本站收录的所有视频，包括B站UV分区下的所有视频和部分其他分区中的VC视频。
-        </li>
-        <li>
-          <router-link to="/sprint">传说助攻</router-link>：VC传说冲刺曲目助攻计划，收录B站接近<a href="https://zh.moegirl.org/Vocaloid中文传说曲" target="_blank">中文VOCALOID传说曲</a>要求的曲目视频，记录播放、收藏等数据变化，提供传说助攻参考。
-        </li>
-        <li>
-          <router-link to="/tool">辅助工具</router-link>：泛VC数据记录与分享相关所需要的辅助工具。
-        </li>
-        <li>开发中...</li>
-      </ul>
+      <h3><router-link to="/video">所有视频</router-link></h3>
+      <p>本站收录的所有视频，包括B站UV分区下的所有视频和部分其他分区中的VC视频。</p>
+      <h3><router-link to="/sprint">传说助攻</router-link></h3>
+      <p>VC传说冲刺曲目助攻计划，收录B站接近<a href="https://zh.moegirl.org/Vocaloid中文传说曲" target="_blank">中文VOCALOID传说曲</a>要求的曲目视频，记录播放、收藏等数据变化，提供传说助攻参考。</p>
+      <h3><router-link to="/tool">辅助工具</router-link></h3>
+      <p>泛VC数据记录与分享相关所需要的辅助工具。</p>
     </div>
   </div>
 </template>
