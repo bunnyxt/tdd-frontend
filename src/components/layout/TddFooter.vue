0ab06c5671adbc172ee1c1645a17b40ec03a065a
bunnyxt <bunnyxt@outlook.com>
2020-01-19 12:49:12 +0800

    Change homepage from http to https.

--- src/components/layout/TddFooter.vue
+++ src/components/layout/TddFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout-footer style="text-align: center; height: 120px">
     TianDian Daily by
-    <a href="http://www.bunnyxt.com" target="_blank">bunnyxt</a>
+    <a href="https://www.bunnyxt.com" target="_blank">bunnyxt</a>
     <br>2017~2019 © All Rights Reserved
     <br>苏ICP备17067742号-1
   </a-layout-footer>
