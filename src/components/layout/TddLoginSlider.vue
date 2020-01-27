3160fa03fd8f60278a3ef78b2a9a5cb3fc0471ce
bunnyxt <bunnyxt@outlook.com>
2020-01-28 01:10:39 +0800

    Create TddLoginSlider and related state in vuex.

---
+++ src/components/layout/TddLoginSlider.vue
@@ -0,0 +1,42 @@
+<template>
+  <a-drawer
+      placement="right"
+      :closable="true"
+      :visible="this.$store.state.isLoginSliderVisible"
+      :width="this.$store.state.clientWidth > 320 ? '320px' : this.$store.state.clientWidth + 'px'"
+      @close="drawerCloseHandler"
+  >
+    <div>
+      <a-menu v-model="currentKeys" mode="horizontal">
+        <a-menu-item key="login">
+          登录
+        </a-menu-item>
+        <a-menu-item key="register">
+          注册
+        </a-menu-item>
+      </a-menu>
+      <div v-if="currentKeys.indexOf('login') !== -1">
+        登录相关表单
+      </div>
+      <div v-if="currentKeys.indexOf('register') !== -1">
+        注册相关表单
+      </div>
+    </div>
+  </a-drawer>
+</template>
+
+<script>
+  export default {
+    name: 'TddLoginSlider',
+    data: function () {
+      return {
+        currentKeys: ['login']
+      }
+    },
+    methods: {
+      drawerCloseHandler: function() {
+        this.$store.commit('changeLoginSliderVisibility');
+      }
+    }
+  }
+</script>
