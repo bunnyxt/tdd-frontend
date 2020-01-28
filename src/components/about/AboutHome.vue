2bf8b97180145e585b5b7d60c393b63e0be3f288
bunnyxt <bunnyxt@outlook.com>
2020-01-29 00:53:30 +0800

    Update website description.

--- src/components/about/AboutHome.vue
+++ src/components/about/AboutHome.vue
@@ -12,7 +12,7 @@
         <img src="img/logo_64S.png" alt="logo" /><br>
         <span style="font-size: 10px">logo by <br>SweetFairyTale</span>
       </div>
-      <p>天钿Daily（TianDian Daily）是一个VC相关数据交流与分享的网站。本站以推动VC相关数据交流为宗旨，基于B站API以及简单的网络爬虫，定期抓取并存储VC相关数据，选取感兴趣的纬度公开展示。</p>
+      <p>天钿Daily（TianDian Daily）是一个VC相关数据交流与分享的网站。本站致力于VC相关数据交流，基于B站API以及简单的网络爬虫，定期抓取VC相关数据，选取有意义的纬度展示。</p>
       <p>
         QQ群：<a target="_blank" href="https://jq.qq.com/?_wv=1027&k=588s7nw">537793686</a><br/>
         个人邮箱：<a href="mailto:bunnyxt@outlook.com">bunnyxt@outlook.com</a>
