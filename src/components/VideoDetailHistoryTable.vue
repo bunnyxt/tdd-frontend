6ddcc3e55d44b667a50d50b400d01fa0721e71ea
bunnyxt <bunnyxt@outlook.com>
2020-02-05 22:38:40 +0800

    Create VideoDetailHistoryTable.vue

---
+++ src/components/VideoDetailHistoryTable.vue
@@ -0,0 +1,71 @@
+<template>
+  <a-table
+      :columns="columns"
+      :rowKey="record => record.added"
+      :dataSource="videoRecords"
+      :pagination="pagination"
+      :scroll="{ x: 700 }"
+      size="small"
+  >
+    <template slot="added" slot-scope="added">
+      {{ $util.tsToDateString(added) }}
+    </template>
+  </a-table>
+</template>
+
+<script>
+  export default {
+    name: 'VideoDetailHistoryTable',
+    props: {
+      videoRecords: {
+        type: Array,
+        required: true
+      }
+    },
+    data: function () {
+      return {
+        columns: [
+          {
+            title: '时间',
+            dataIndex: 'added',
+            scopedSlots: { customRender: 'added' },
+            sorter: (a, b) => a.added - b.added,
+            defaultSortOrder: 'descend',
+            width: '160px',
+            fixed: 'left',
+          }, {
+            title: '播放',
+            dataIndex: 'view',
+          }, {
+            title: '弹幕',
+            dataIndex: 'danmaku',
+          }, {
+            title: '评论',
+            dataIndex: 'reply',
+          }, {
+            title: '收藏',
+            dataIndex: 'favorite',
+          }, {
+            title: '硬币',
+            dataIndex: 'coin',
+          }, {
+            title: '分享',
+            dataIndex: 'share',
+          }, {
+            title: '点赞',
+            dataIndex: 'like',
+          }
+        ],
+        pagination: {
+          size: 'big',
+          showQuickJumper: true,
+          showTotal: total => `共 ${total} 条记录`
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
