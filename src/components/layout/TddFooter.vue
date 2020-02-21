12b43eec3f845c26fb1862943519e8da08954a3f
bunnyxt <bunnyxt@outlook.com>
2020-02-21 21:08:57 +0800

    Add info to footer.

--- src/components/layout/TddFooter.vue
+++ src/components/layout/TddFooter.vue
@@ -1,14 +1,29 @@
 <template>
-  <a-layout-footer style="text-align: center; height: 120px">
-    TianDian Daily by
-    <a href="https://www.bunnyxt.com" target="_blank">bunnyxt</a>
-    <br>2017~2020 © All Rights Reserved
-    <br>苏ICP备17067742号-1
+  <a-layout-footer class="tdd-footer">
+    天钿Daily by
+    <a href="https://www.bunnyxt.com" target="_blank">bunnyxt</a><br/>
+    <a href="https://api.bunnyxt.com/tdd/doc" target="_blank">接口文档</a> |
+    <a href="https://github.com/users/bunnyxt/projects/1" target="_blank">项目源码</a> |
+    <a href="https://jq.qq.com/?_wv=1027&k=588s7nw" target="_blank">官方QQ群</a>
+    <br/>
+    2017~2020 © All Rights Reserved<br/>
+    <a href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action" target="_blank">苏ICP备17067742号-1</a>
   </a-layout-footer>
 </template>
 
 <script>
-export default {
-  name: "TddFooter"
-};
+  export default {
+    name: "TddFooter"
+  };
 </script>
+
+<style scoped>
+  .tdd-footer {
+    text-align: center;
+    height: 120px;
+    font-size: 12px;
+  }
+  a {
+    color: rgba(0, 0, 0, 0.65);
+  }
+</style>
