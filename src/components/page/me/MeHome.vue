ad54e005e781215c6be628fa844379420b6ba5a3
bunnyxt <bunnyxt@outlook.com>
2020-03-02 15:02:07 +0800

    Add sign in button.

--- src/components/page/me/MeHome.vue
+++ src/components/page/me/MeHome.vue
@@ -38,7 +38,22 @@
               </div>
             </div>
             <div style="float: right">
-              签到相关
+              <a-tooltip placement="bottomRight">
+                <template slot="title">
+<!--                  <span>TODO 加个日历  可以查</span>-->
+                  <div v-if="isLoadingUserSignInOverview || isLoadingUserSignInList">
+                    <a-spin />
+                  </div>
+                  <div v-else>
+                    {{ '历史签到' + userSignInOverview.total + '天' }}<br>
+                    {{ tmpSignInTooltipStr }}
+                  </div>
+                </template>
+                <a-button
+                    @click="goSignIn"
+                    :disabled="userSignInStatusToday"
+                >{{ userSignInStatusToday ? '已签到' : '签到' }}</a-button>
+              </a-tooltip>
             </div>
           </div>
         </div>
@@ -59,13 +74,19 @@
 
 <script>
   import md5 from 'js-md5';
+  import moment from 'moment';
 
   export default {
     name: 'MeHome',
     data: function () {
       return {
         isLoadingUserInfo: false,
-        user: null
+        user: {},
+        isLoadingUserSignInList: false,
+        userSignInList: [],
+        isLoadingUserSignInOverview: false,
+        userSignInOverview: {},
+        isGoingSignIn: false
       }
     },
     computed: {
@@ -80,9 +101,28 @@
         } else {
           return 'https://www.gravatar.com/avatar/' + md5('anonymous@tdd.bunnyxt.com') + '?d=identicon';
         }
+      },
+      userSignInStatusToday: function () {
+        if (this.userSignInOverview) {
+          if (this.userSignInOverview.last_added >= Math.floor(moment().startOf('day').valueOf() / 1000)) {
+            return true;
+          }
+        }
+        return false;
+      },
+      tmpSignInTooltipStr: function () {
+        let str = '';
+        if (this.userSignInStatusToday) {
+          let totaySignIn = this.userSignInList[this.userSignInList.length - 1];
+          str += '今日已签到，第' + totaySignIn.rank + '名';
+        } else {
+          str += '今日未签到';
+        }
+        return str;
       }
     },
     methods: {
+      // TODO use promise to rewrite
       fetchUserInfo: function () {
         this.isLoadingUserInfo = true;
 
@@ -111,6 +151,8 @@
 
                 // go to home page
                 that.$router.push('/');
+              } else {
+                console.log(error.response);
               }
             } else {
               console.log(error);
@@ -118,6 +160,129 @@
           })
           .finally(function () {
             that.isLoadingUserInfo = false;
+          });
+      },
+      fetchUserSignInList: function () {
+        this.isLoadingUserSignInList = true;
+
+        let that = this;
+        this.$axios.get('/user/signin/me')
+          .then(function (response) {
+            that.userSignInList = response.data;
+          })
+          .catch(function (error) {
+            if (error.response) {
+              if (error.response.data.code === 40102) {
+                // user not logged in
+
+                // clear local storage
+                localStorage.removeItem('tddUserDetail');
+
+                // set status
+                that.$store.commit('setUserLoginStatus', false);
+                that.$store.commit('setUserDetail', null);
+
+                that.$message.warn('用户登录失效，请重新登录');
+
+                // go to home page
+                that.$router.push('/');
+              } else {
+                console.log(error.response);
+              }
+            } else {
+              console.log(error);
+            }
+          })
+          .finally(function () {
+            that.isLoadingUserSignInList = false;
+          });
+      },
+      fetchUserSignInOverview: function () {
+        this.isLoadingUserSignInOverview = true;
+
+        let that = this;
+        this.$axios.get('/user/signin/overview/me')
+          .then(function (response) {
+            that.userSignInOverview = response.data;
+          })
+          .catch(function (error) {
+            if (error.response) {
+              if (error.response.data.code === 40102) {
+                // user not logged in
+
+                // clear local storage
+                localStorage.removeItem('tddUserDetail');
+
+                // set status
+                that.$store.commit('setUserLoginStatus', false);
+                that.$store.commit('setUserDetail', null);
+
+                that.$message.warn('用户登录失效，请重新登录');
+
+                // go to home page
+                that.$router.push('/');
+              } else {
+                console.log(error.response);
+              }
+            } else {
+              console.log(error);
+            }
+          })
+          .finally(function () {
+            that.isLoadingUserSignInOverview = false;
+          });
+      },
+      goSignIn: function () {
+        this.isGoingSignIn = true;
+        let that = this;
+        this.$axios.post('/user/signin')
+          .then(function (response) {
+            const resp = response.data;
+            if (resp.status === 'success') {
+              that.$message.info(`签到成功！今日第${resp.detail.rank}名`);
+              that.$message.info(`连续签到${resp.detail.consecutive_days}天，积分+${resp.detail.point}，经验+${resp.detail.exp}`);
+
+              // update info
+              that.fetchUserInfo();
+              that.fetchUserSignInList();
+              that.fetchUserSignInOverview();
+            } else if (resp.status === 'fail') {
+              if (resp.message === 'user have already signed in today') {
+                that.$message.warn('今天已经签过到了——');
+              } else {
+                that.$message.error('签到失败');
+                that.$message.error(resp);
+              }
+            } else {
+              that.$message.error('签到失败');
+              that.$message.error(resp);
+            }
+          })
+          .catch(function (error) {
+            if (error.response) {
+              if (error.response.data.code === 40102) {
+                // user not logged in
+
+                // clear local storage
+                localStorage.removeItem('tddUserDetail');
+
+                // set status
+                that.$store.commit('setUserLoginStatus', false);
+                that.$store.commit('setUserDetail', null);
+
+                that.$message.warn('用户登录失效，请重新登录');
+
+                // go to home page
+                that.$router.push('/');
+              } else {
+                console.log(error.response);
+              }
+            } else {
+              console.log(error);
+            }
+          })
+          .finally(function () {
+            that.isGoingSignIn = false;
           });
       },
       getRoleTitleColor: function (title) {
@@ -138,6 +303,8 @@
     },
     created() {
       this.fetchUserInfo();
+      this.fetchUserSignInList();
+      this.fetchUserSignInOverview();
     }
   }
 </script>
