88468270eaf211c5307fbccffa88e4741fae9807
bunnyxt <bunnyxt@outlook.com>
2020-03-24 14:18:04 +0800

    Add convert via algorithm.

--- src/components/page/tool/AidBvidConvert.vue
+++ src/components/page/tool/AidBvidConvert.vue
@@ -10,6 +10,12 @@
     <div class="section-block">
       <h1>Bilibili av号/bv号转换工具</h1>
       <div style="overflow: hidden; margin-bottom: 12px">
+        <a-radio-group v-model="method" buttonStyle="solid">
+          <a-radio-button value="api">API转换</a-radio-button>
+          <a-radio-button value="alg">算法转换</a-radio-button>
+        </a-radio-group>
+      </div>
+      <div style="overflow: hidden; margin-bottom: 12px">
         <a-select
             defaultValue="aid"
             v-model="queryKey"
@@ -31,10 +37,12 @@
         <a-input style="float: left; width: 140px; margin-left: 12px" :value="resultValue" />
       </div>
       <div>
-        注意：本在线工具仅适用于小规模普通查询，大规模查询请直接调用Bilibili API
+        注意：<br>
+        API转换调用B站API进行转换，仅适用于小规模普通查询，大规模查询请直接调用Bilibili API
         <a :href="'https://api.bilibili.com/x/web-interface/view?'+queryKey+'='+queryValue" target="_blank">
           {{ 'https://api.bilibili.com/x/web-interface/view?'+queryKey+'='+queryValue }}
-        </a>
+        </a><br>
+        算法转换采用知乎用户<a href="https://www.zhihu.com/people/-._.-" target="_blank">mcfx</a>在<a href="https://www.zhihu.com/people/-._.-" target="_blank">这篇回答</a>中给出的算法进行转换
       </div>
     </div>
   </div>
@@ -45,9 +53,10 @@
     name: 'AidBvidConvert',
     data: function () {
       return {
+        method: 'api',
         queryKey: 'aid',
         queryValue: '456930',
-        queryResultObj: {status:"success",aid:456930,bvid:"BV19x411F7kL"},
+        queryResultObj: {status:"success",aid:456930,bvid:"BV19x411F7kL"}
       }
     },
     computed: {
@@ -79,16 +88,88 @@
         }
       },
       queryClickHandler() {
-        this.$axios.get('https://api.bunnyxt.com/tdd/bapi_aid_bvid.php?'+this.queryKey+'='+this.queryValue)
-          .then((response) => {
-            this.queryResultObj = response.data;
-            if (response.data.status === 'success') {
-              this.$message.success('转换成功');
-            } else {
-              this.$message.error('转换出错');
+        if (this.method === 'api') {
+          this.$axios.get('https://api.bunnyxt.com/tdd/bapi_aid_bvid.php?'+this.queryKey+'='+this.queryValue)
+            .then((response) => {
+              this.queryResultObj = response.data;
+              if (response.data.status === 'success') {
+                this.$message.success('转换成功');
+              } else {
+                this.$message.error('转换出错');
+              }
+            })
+            .catch(() => this.$message.error('请求出错'))
+        } else if (this.method === 'alg') {
+          // ref: https://www.zhihu.com/question/381784377
+          let table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
+          let tr = {};
+          for (let i = 0; i < 58; i++) {
+            tr[table[i]] = i;
+          }
+          let s = [11, 10, 3, 8, 4, 6];
+          let xor = 177451812;
+          let add = 8728348608;
+
+          if (this.queryKey === 'aid') {
+            // enc
+            let x;
+            try {
+              x = parseInt(this.queryValue)
+            } catch (e) {
+              this.$message.error('转换出错，aid必须为数字');
+              return
             }
-          })
-          .catch(() => this.$message.error('请求出错'))
+            x = (x ^ xor) + add;
+            let r = ['B', 'V', '1', ' ', ' ', '4', ' ', '1', ' ', '7', ' ', ' '];
+            for (let i = 0; i < 6; i++) {
+              r[s[i]] = table.charAt(Math.floor(x / Math.pow(58, i)) % 58);
+            }
+            let rs = '';
+            for (let i = 0; i < r.length; i++) {
+              rs += r[i];
+            }
+            this.queryResultObj = {
+              status: 'success',
+              aid: this.queryValue,
+              bvid: rs
+            };
+            this.$message.success('转换成功')
+          } else if (this.queryKey === 'bvid') {
+            // dec
+            let r = 0;
+            let x = [];
+            for (let i = 0; i < this.queryValue.length; i++) {
+              x.push(this.queryValue.charAt(i));
+            }
+            if (x.length !== 12) {
+              this.$message.error('转换出错，bvid位数不合法');
+              return
+            }
+            if (!(x[0] === 'B' && x[1] === 'V')) {
+              this.$message.error('转换出错，bvid必须以BV开头');
+              return
+            }
+            if (!(x[2] === '1' && x[5] === '4' && x[7] === '1' && x[9] === '7')) {
+              this.$message.error('转换出错，目前只支持BV1??4?1?7??格式的bvid');
+              return
+            }
+            for (let i = 2; i < 12; i++) {
+              if (table.indexOf(x[i]) === -1) {
+                this.$message.error('转换出错，bvid包含不支持的字符');
+                return
+              }
+            }
+            for (let i = 0; i < 6; i++) {
+              r += tr[x[s[i]]] * Math.pow(58, i);
+            }
+            this.queryResultObj = {
+              status: 'success',
+              aid: String((r - add) ^ xor),
+              bvid: this.queryValue
+            };
+            this.$message.success('转换成功')
+          }
+        }
       }
     }
   }
