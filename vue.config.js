f251e56bab889c16b74e321412f6d0ddda49451a
bunnyxt <bunnyxt@outlook.com>
2020-08-18 16:47:23 +0800

    Add BundleAnalyzerPlugin.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerPort: 8889
+      })
+    ]
+  }
 }
