c76513315e6072c0096f1ddc789d0d422c0b300b
bunnyxt <bunnyxt@outlook.com>
2020-08-30 12:38:55 +0800

    Finish tdd video history line chart.

--- src/components/chart/TddVideoHistoryLineChart.vue
+++ src/components/chart/TddVideoHistoryLineChart.vue
@@ -9,65 +9,65 @@
         </div>
         <span style="cursor: pointer"><a-icon type="setting" /> 图表设置</span>
       </a-popover>
-<!--      <div v-if="$store.getters.clientMode === 'MOBILE'" style="float: right" @click="onAddedRangeMobileClick">-->
-<!--        <span style="cursor: pointer">时间范围 <a-icon type="calendar" /></span>-->
-<!--        <a-modal title="时间范围" v-model="addedRangeMobileVisibility">-->
-<!--          <template slot="footer">-->
-<!--            <a-button type="primary" @click="onAddedRangeMobileClick">确定</a-button>-->
-<!--          </template>-->
-<!--          <p>-->
-<!--            开始：-->
-<!--            <a-date-picker-->
-<!--              showTime-->
-<!--              format="YYYY-MM-DD HH:mm:ss"-->
-<!--              placeholder="开始"-->
-<!--              v-model="addedRangeValueStart"-->
-<!--              @change="onAddedRangeValueStartChange"-->
-<!--            ></a-date-picker>-->
-<!--          </p>-->
-<!--          <p>-->
-<!--            结束：-->
-<!--            <a-date-picker-->
-<!--              showTime-->
-<!--              format="YYYY-MM-DD HH:mm:ss"-->
-<!--              placeholder="结束"-->
-<!--              v-model="addedRangeValueEnd"-->
-<!--              @change="onAddedRangeValueEndChange"-->
-<!--            ></a-date-picker>-->
-<!--          </p>-->
-<!--          <p>-->
-<!--            <a-button size="small" @click="setAddedRangeMobile(1)" style="margin-right: 4px">1日</a-button>-->
-<!--            <a-button size="small" @click="setAddedRangeMobile(7)" style="margin-right: 4px">7日</a-button>-->
-<!--            <a-button size="small" @click="setAddedRangeMobile(30)" style="margin-right: 4px">30日</a-button>-->
-<!--            <a-button size="small" @click="setAddedRangeMobile(180)">180日</a-button>-->
-<!--          </p>-->
-<!--          <p>数据共计：{{ this.videoRecords.length }}条，当前展示：{{ this.data.length }}条。</p>-->
-<!--          <a-alert v-if="data.length > 200" message="当前显示数据较多，可能会出现图表卡顿，如欲提高响应速度，请酌情缩小时间范围。" banner />-->
-<!--        </a-modal>-->
-<!--      </div>-->
-<!--      <template v-else>-->
-<!--        <a-popover title="时间范围" trigger="click" placement="bottomRight" style="float: right">-->
-<!--          <template slot="content">-->
-<!--            <p>-->
-<!--              <a-range-picker-->
-<!--                showTime-->
-<!--                v-model="addedRangeValue"-->
-<!--                :ranges="addedRangeRanges"-->
-<!--                :disabledDate="addedRangeDisabledDate"-->
-<!--                @change="onAddedRangeChange"-->
-<!--                style="width: 360px"-->
-<!--              />-->
-<!--            </p>-->
-<!--            <p>数据共计：{{ this.videoRecords.length }}条，当前展示：{{ this.data.length }}条。</p>-->
-<!--            <a-alert v-if="data.length > 200" banner>-->
-<!--              <template slot="message">-->
-<!--                当前显示数据较多，可能会出现图表卡顿，<br>如欲提高响应速度，请酌情缩小时间范围。-->
-<!--              </template>-->
-<!--            </a-alert>-->
-<!--          </template>-->
-<!--          <span style="cursor: pointer">时间范围 <a-icon type="calendar" /></span>-->
-<!--        </a-popover>-->
-<!--      </template>-->
+      <div v-if="$store.getters.clientMode === 'MOBILE'" style="float: right" @click="onAddedRangeMobileClick">
+        <span style="cursor: pointer">时间范围 <a-icon type="calendar" /></span>
+        <a-modal title="时间范围" v-model="addedRangeMobileVisibility">
+          <template slot="footer">
+            <a-button type="primary" @click="onAddedRangeMobileClick">确定</a-button>
+          </template>
+          <p>
+            开始：
+            <a-date-picker
+              showTime
+              format="YYYY-MM-DD HH:mm:ss"
+              placeholder="开始"
+              v-model="addedRangeValueStart"
+              @change="onAddedRangeValueStartChange"
+            ></a-date-picker>
+          </p>
+          <p>
+            结束：
+            <a-date-picker
+              showTime
+              format="YYYY-MM-DD HH:mm:ss"
+              placeholder="结束"
+              v-model="addedRangeValueEnd"
+              @change="onAddedRangeValueEndChange"
+            ></a-date-picker>
+          </p>
+          <p>
+            <a-button size="small" @click="setAddedRangeMobile(1)" style="margin-right: 4px">1日</a-button>
+            <a-button size="small" @click="setAddedRangeMobile(7)" style="margin-right: 4px">7日</a-button>
+            <a-button size="small" @click="setAddedRangeMobile(30)" style="margin-right: 4px">30日</a-button>
+            <a-button size="small" @click="setAddedRangeMobile(180)">180日</a-button>
+          </p>
+          <p>数据共计：{{ this.videoRecords.length }}条，当前展示：{{ this.filteredData.length }}条。</p>
+          <a-alert v-if="this.filteredData.length > 200" message="当前显示数据较多，可能会出现图表卡顿，如欲提高响应速度，请酌情缩小时间范围。" banner />
+        </a-modal>
+      </div>
+      <template v-else>
+        <a-popover title="时间范围" trigger="click" placement="bottomRight" style="float: right">
+          <template slot="content">
+            <p>
+              <a-range-picker
+                showTime
+                v-model="addedRangeValue"
+                :ranges="addedRangeRanges"
+                :disabledDate="addedRangeDisabledDate"
+                @change="onAddedRangeChange"
+                style="width: 380px"
+              />
+            </p>
+            <p>数据共计：{{ this.videoRecords.length }}条，当前展示：{{ this.filteredData.length }}条。</p>
+            <a-alert v-if="this.filteredData.length > 200" banner>
+              <template slot="message">
+                当前显示数据较多，可能会出现图表卡顿，<br>如欲提高响应速度，请酌情缩小时间范围。
+              </template>
+            </a-alert>
+          </template>
+          <span style="cursor: pointer">时间范围 <a-icon type="calendar" /></span>
+        </a-popover>
+      </template>
     </div>
     <div id="tdd-video-history-line-chart"></div>
   </div>
@@ -76,6 +76,7 @@
 <script>
 import { Chart } from '@antv/g2';
 import { DataView } from '@antv/data-set';
+import moment from "moment";
 
 export default {
   name: 'TddVideoHistoryLineChart',
@@ -91,76 +92,57 @@ export default {
   },
   data: function () {
     return {
+      // chart
       chart: null,
       axisType: 'linear',
       chartPaddingDESKTOP: [ 20, 8, 95, 68 ],
       chartPaddingMOBILE: [ 20, 8, 50, 8 ],
       chartHeightDESKTOP: 400,
       chartHeightMOBILE: 300,
+      // left panel, chart config setting
       showBasePoint: false,
       showSpecialPoint: true,
+      // right panel, added range setting
+      addedRangeValue: [
+        moment().subtract(7, 'days'),
+        moment()
+      ],
+      addedRangeRanges: {
+        '1日': [moment().subtract(1, 'days'), moment()],
+        '7日': [moment().subtract(7, 'days'), moment()],
+        '30日': [moment().subtract(30, 'days'), moment()],
+        '180日': [moment().subtract(180, 'days'), moment()],
+      },
+      addedRangeDisabledDate: current => current > moment().endOf('day'),
+      addedRangeMobileVisibility: false,
+      addedRangeValueStart: null,
+      addedRangeValueEnd: null,
     }
   },
   computed: {
-    compact: function() {
-      return this.$store.getters.clientMode === 'MOBILE';
-    },
-    chartPadding: function() {
-      return this.compact
-        ? this.chartPaddingMOBILE
-        : this.chartPaddingDESKTOP;
-    },
-    chartHeight: function() {
-      return this.compact
-        ? this.chartHeightMOBILE
-        : this.chartHeightDESKTOP;
-    }
-  },
-  watch: {
-    compact: function() {
-      this.chart.destroy();
-      this.draw();
-    },
-    videoRecords: function () {
-      this.chart.changeData(this.transformRecords(this.videoRecords, this.pubdate, undefined, undefined, this.showBasePoint));
-    }
-  },
-  methods: {
-    toggleAxisType: function (checked) {
-      if (checked) {
-        this.axisType = 'log';
+    startTs: function () {
+      if (this.addedRangeValue[0]) {
+        return Math.floor(this.addedRangeValue[0].valueOf() / 1000);
       } else {
-        this.axisType = 'linear';
+        // return ((new Date()).valueOf() / 1000 - 7 * 24 * 60 * 60).toFixed(0);
+        return 0;
       }
-      this.setConfig(this.compact);
-      this.chart.render();
     },
-    toggleShowBasePoint: function (checked) {
-      this.showBasePoint = checked;
-      this.chart.changeData(this.transformRecords(this.videoRecords, this.pubdate,
-        undefined, undefined, this.showBasePoint));
-    },
-    toggleShowSpecialPoint: function (checked) {
-      this.showSpecialPoint = checked;
-      if (this.showSpecialPoint) {
-        this.setAnnotation();
+    endTs: function (){
+      if (this.addedRangeValue[1]) {
+        return Math.floor(this.addedRangeValue[1].valueOf() / 1000);
       } else {
-        this.chart.annotation().clear(true);
+        return ((new Date()).valueOf() / 1000).toFixed(0);
       }
-      this.chart.render();
     },
-    transformRecords: function (rawRecords,
-                                pubdate = 0,
-                                startTs = 0,
-                                endTs = ((new Date()).valueOf() / 1000).toFixed(0),
-                                showBasePoint = false) {
-      let newRecords = [];
-
+    filteredData: function () {
+      let filteredRecords = [];
+      
       // add base point
-      if (showBasePoint) {
-        newRecords.push({
+      if (this.showBasePoint) {
+        filteredRecords.push({
           id: 0,
-          added: pubdate,
+          added: this.pubdate,
           view: 0,
           danmaku: 0,
           reply: 0,
@@ -172,28 +154,32 @@ export default {
       }
       
       // cut via range
-      newRecords = [
-        ...newRecords,
-        ...rawRecords.filter(v => v.added >= startTs && v.added <= endTs)
+      filteredRecords = [
+        ...filteredRecords,
+        ...this.videoRecords.filter(v => v.added >= this.startTs && v.added <= this.endTs)
       ]
-
+      
+      return filteredRecords;
+    },
+    transformedData: function () {
+      const filteredData = [...this.filteredData];
       // add view speed
-      if (newRecords.length > 0) {
-        newRecords[0].view_speed = 0; // set first speed to 0
-        for (let i = 1; i < newRecords.length; i++) {
-          const view_diff = newRecords[i].view - newRecords[i-1].view;
-          const added_diff = newRecords[i].added - newRecords[i-1].added;
+      if (filteredData.length > 0) {
+        filteredData[0].view_speed = 0; // set first speed to 0
+        for (let i = 1; i < filteredData.length; i++) {
+          const view_diff = filteredData[i].view - filteredData[i-1].view;
+          const added_diff = filteredData[i].added - filteredData[i-1].added;
           if (view_diff === 0 && added_diff <= 75) {
             // two record with same value in 75s
-            newRecords[i].view_speed = newRecords[i-1].view_speed;
+            filteredData[i].view_speed = filteredData[i-1].view_speed;
           } else {
             // speed per hour
-            newRecords[i].view_speed = parseFloat((view_diff / added_diff * 60 * 60).toFixed(2));
+            filteredData[i].view_speed = parseFloat((view_diff / added_diff * 60 * 60).toFixed(2));
           }
         }
       }
-
-      return new DataView().source(newRecords)
+      
+      return new DataView().source(filteredData)
         .transform({
           type: 'rename',
           map: {
@@ -221,6 +207,95 @@ export default {
           }
         }).rows;
     },
+    compact: function() {
+      return this.$store.getters.clientMode === 'MOBILE';
+    },
+    chartPadding: function() {
+      return this.compact
+        ? this.chartPaddingMOBILE
+        : this.chartPaddingDESKTOP;
+    },
+    chartHeight: function() {
+      return this.compact
+        ? this.chartHeightMOBILE
+        : this.chartHeightDESKTOP;
+    }
+  },
+  watch: {
+    compact: function() {
+      this.chart.destroy();
+      this.draw();
+    },
+    videoRecords: function () {
+      this.chart.changeData(this.transformedData);
+    }
+  },
+  methods: {
+    // left panel, chart config setting
+    toggleAxisType: function (checked) {
+      if (checked) {
+        this.axisType = 'log';
+      } else {
+        this.axisType = 'linear';
+      }
+      this.setConfig(this.compact);
+      this.chart.render();
+    },
+    toggleShowBasePoint: function (checked) {
+      this.showBasePoint = checked;
+      this.chart.changeData(this.transformedData);
+    },
+    toggleShowSpecialPoint: function (checked) {
+      this.showSpecialPoint = checked;
+      if (this.showSpecialPoint) {
+        this.setAnnotation();
+      } else {
+        this.chart.annotation().clear(true);
+      }
+      this.chart.render();
+    },
+    // right panel, added range setting
+    onAddedRangeChange: function () {
+      this.chart.changeData(this.transformedData);
+    },
+    onAddedRangeMobileClick: function () {
+      this.addedRangeMobileVisibility = !this.addedRangeMobileVisibility;
+    },
+    onAddedRangeValueStartChange: function () {
+      if (this.addedRangeValueStart === null) {
+        this.addedRangeValueEnd = null;
+        this.addedRangeValue = [];
+      } else {
+        if (this.addedRangeValue.length !== 2) {
+          this.addedRangeValueEnd = moment();
+          this.addedRangeValue = [this.addedRangeValueStart, moment()];
+        } else {
+          this.addedRangeValue = [this.addedRangeValueStart, this.addedRangeValue[1]];
+        }
+      }
+      this.onAddedRangeChange();
+    },
+    onAddedRangeValueEndChange: function () {
+      if (this.addedRangeValueEnd === null) {
+        this.addedRangeValueStart = null;
+        this.addedRangeValue = [];
+      } else {
+        if (this.addedRangeValue.length !== 2) {
+          this.addedRangeValueStart = moment(0);
+          this.addedRangeValue = [moment(0), this.addedRangeValueEnd];
+        } else {
+          this.addedRangeValue = [this.addedRangeValue[0], this.addedRangeValueEnd];
+        }
+      }
+      this.onAddedRangeChange();
+    },
+    setAddedRangeMobile: function (day) {
+      this.addedRangeValueStart = moment().subtract(day, 'days');
+      this.addedRangeValueEnd = moment();
+      this.addedRangeValue = [this.addedRangeValueStart, this.addedRangeValueEnd];
+      this.onAddedRangeChange();
+    },
+    // chart draw
     createChart: function () {
       this.chart = new Chart({
         container: 'tdd-video-history-line-chart',
@@ -229,10 +304,10 @@ export default {
         padding: this.chartPadding,
       });
     },
-    setData: function (data) {
-      this.chart.data(data);
+    setData: function () {
+      this.chart.data(this.transformedData);
     },
-    setConfig: function (compact) {
+    setConfig: function () {
       this.chart.scale({
         added: {
           type: 'time',
@@ -259,7 +334,7 @@ export default {
         }
       });
       this.chart.axis('value', {
-        label: compact ? null : {
+        label: this.compact ? null : {
           formatter: val => parseInt(val).toLocaleString()
         }
       });
@@ -268,7 +343,7 @@ export default {
         grid: null
       });
       
-      if (!compact) {
+      if (!this.compact) {
         // TODO update to newest version to fix this bug
         this.chart.option('slider', {
           height: -30,
@@ -281,7 +356,7 @@ export default {
       }
     },
     setAnnotation: function () {
-      const data = this.videoRecords;
+      const data = this.filteredData;
       if (data.length === 0) {
         return;
       }
@@ -370,9 +445,8 @@ export default {
     },
     draw: function () {
       this.createChart();
-      this.setData(this.transformRecords(this.videoRecords, this.pubdate,
-        undefined, undefined, this.showBasePoint));
-      this.setConfig(this.compact);
+      this.setData();
+      this.setConfig();
       this.setAnnotation();
       this.setLayout();
       this.chart.render();
