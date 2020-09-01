490a56cf6dd2f7eb482251387a5a38058c474d17
bunnyxt <bunnyxt@outlook.com>
2020-09-01 13:45:05 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://api.bunnyxt.com/tdd/v2/';
-axios.defaults.baseURL = 'http://localhost:1437/';
+axios.defaults.baseURL = 'https://api.bunnyxt.com/tdd/v2/';
+// axios.defaults.baseURL = 'http://localhost:1437/';
 
 axios.defaults.withCredentials = true;
 
