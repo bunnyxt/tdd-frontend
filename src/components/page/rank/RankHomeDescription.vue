aa7b1c1de3f1c44141806b4d94ed732d4aeeace7
bunnyxt <bunnyxt@outlook.com>
2020-12-08 18:04:13 +0800

    Fix lines with line-x class.

--- src/components/page/rank/RankHomeDescription.vue
+++ src/components/page/rank/RankHomeDescription.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    开始时间：{{ $util.tsToDateString(startTs) }}<br/>
-    统计时间：{{ $util.tsToDateString(endTs) }}<br/>
-    *周榜统计时间范围与周刊同步，北京时间每周六凌晨三点归档当前期数据，开始新一期排行<br/>
-    *实时数据每小时更新一次，存在几分钟更新延迟
+    <div class="line-1">开始时间：{{ $util.tsToDateString(startTs) }}</div>
+    <div class="line-1">统计时间：{{ $util.tsToDateString(endTs) }}</div>
+    <div class="line-2" style="margin-top: 8px">*周榜统计时间范围与周刊同步，北京时间每周六凌晨三点归档当前期数据，开始新一期排行</div>
+    <div class="line-1">*实时数据每小时更新一次，存在几分钟更新延迟</div>
   </div>
 </template>
 
