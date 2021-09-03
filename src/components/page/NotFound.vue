6eef3d0aa025586f45af8989da97cd8273f582f1
bunnyxt <bunnyxt@outlook.com>
2021-09-02 21:56:31 -0700

    feat: add i18n for 404 page

--- src/components/page/NotFound.vue
+++ src/components/page/NotFound.vue
@@ -1,11 +1,24 @@
+<i18n>
+{
+  "zh": {
+    "back_to_home": "返回首页",
+    "back_to_previous_page": "返回上一页"
+  },
+  "en": {
+    "back_to_home": "Back to Home",
+    "back_to_previous_page": "Back to Previous Page"
+  }
+}
+</i18n>
+
 <template>
   <div>
     <div class="section-block" :style="sectionBlockStyle">
       <h1>404 NOT FOUND</h1>
       <a-icon type="home" style="margin-right: 8px"/>
-      <router-link to="/">返回首页</router-link>
+      <router-link to="/">{{ $t('back_to_home') }}</router-link>
       <a-icon type="rollback" style="margin-left: 24px; margin-right: 8px"/>
-      <a @click="$router.go(-1)">返回上一页</a>
+      <a @click="$router.go(-1)">{{ $t('back_to_previous_page') }}</a>
     </div>
   </div>
 </template>
