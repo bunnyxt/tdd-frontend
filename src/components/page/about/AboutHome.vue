38b52bfb8704c40f3c5ead026b96472f66406250
bunnyxt <bunnyxt@outlook.com>
2021-09-20 22:50:31 -0700

    feat: add i18n for about part

--- src/components/page/about/AboutHome.vue
+++ src/components/page/about/AboutHome.vue
@@ -1,24 +1,87 @@
+<i18n src="@/i18n/common.json"></i18n>
+<i18n>
+{
+  "zh": {
+    "about_tdd": "关于天钿Daily",
+    "tdd_intro": "天钿Daily（TianDian Daily）是一个VC相关数据交流与分享的网站。本站致力于VC相关数据交流，基于B站API以及简单的网络爬虫，定期抓取VC相关数据，选取有意义的纬度展示。",
+    "qq_group_info": "QQ群：{0} {1} {2}",
+    "personal_email_info": "个人邮箱：{0}",
+    "donate_tdd": "资助本站",
+    "tech_related": "技术相关",
+    "tech_server": "小霸王服务器目前由阿里云与腾讯云提供，{0}。",
+    "tech_server_no_hack": "谢绝爆破",
+    "tech_frontend": "前端使用Vue全家桶构建，包括vue-router前端路由以及vuex状态管理。界面使用{0} UI设计语言，并根据项目需要，针对移动端访问进行了一系列优化。图表采用Ant Vision {1}可视化引擎。",
+    "tech_backend": "后端使用Spring Boot构建RESTful API，另外还有部分使用PHP实现的旧版接口。本站所有数据可以通过调用API获取。{0}数据库依旧使用MySQL。",
+    "tech_backend_api_limit": "资源有限，请勿滥用。",
+    "tech_crawler": "爬虫脚本基于Python。很丢人，目前还没使用多线程爬虫，不知道怎么避免触发412。",
+    "tech_code": "绝大部分源码可以在{0}看到。点开右侧Menu，进入Linked repositories查看不同模块的项目。{1}",
+    "tech_code_welcome": "欢迎Star，欢迎Issue！",
+    "disclaimer": "免责声明",
+    "disclaimer_source": "本站数据收集自BiliBili。",
+    "disclaimer_privacy": "本站收集的所有数据均从公开渠道获取，不会收集任何敏感信息。",
+    "disclaimer_reliability": "由于存在不可避免的网络问题或程序问题，本站无法保证所有数据的绝对可靠。若您因为本站的信息直接或间接蒙受任何损失，本站将不负任何责任。",
+    "disclaimer_comment": "本站对所有数据持中立态度，不发表也不鼓励基于数据的，对任何实体，包括作品、P主、社团、歌姬等的任何片面评价。数据受到多方面因素的影响，实体的评价也应该综合全面。",
+    "about_bunnyxt": "关于bunnyxt",
+    "bunnyxt_intro": "12年入坑的VC厨。洛厨，南北党。",
+    "bunnyxt_social_media": "日常活跃于新浪微博",
+    "bunnyxt_github": "INTJ, Github "
+  },
+  "en": {
+    "about_tdd": "About TianDian Daily",
+    "tdd_intro": "TianDian Daily is a VC-related data exchange and sharing website. Committed to enhance VOCALOID CHINA related data exchange, fetching VC related data regularly and show meaningful parts to everyone.",
+    "qq_group_info": "QQ group: {0} {1} {2}",
+    "personal_email_info": "Personal email: {0}",
+    "donate_tdd": "Donate TDD",
+    "tech_related": "Tech Related",
+    "tech_server": "Server currently provided by Aliyun and Tencent Cloud. {0}.",
+    "tech_server_no_hack": "PLZ DO NO HACKING",
+    "tech_frontend": "The frontend part is built with Vue family, including vue-router and vuex. UI is based on {0}, and optimized for mobile version. Chart is powered by Ant Vision {1}.",
+    "tech_backend": "The backend part is built with Spring Boot, which generates mostly RESTful APIs. Also some old version APIs by PHP are still available. All data collected can be fetched using provided APIs. {0} TDD uses MySQL for data storage.",
+    "tech_backend_api_limit": "RESOURCES LIMITED, DO NOT ABUSE.",
+    "tech_crawler": "The crawlers are developed using Python. Due to Bilibili API anti-abusing strategies, no multi-threading spiders are used.",
+    "tech_code": "Most source code can be found at {0}. Click Menu at right side, enter Linked repositories to view repos of different parts of TDD. {1}",
+    "tech_code_welcome": "Stars and Issues are welcome!",
+    "disclaimer": "Disclaimer",
+    "disclaimer_source": "TDD data are collected from BiliBili.",
+    "disclaimer_privacy": "TDD only collect data from open sources. No privacy data will be collected.",
+    "disclaimer_reliability": "Due to inevitable network problems or program bugs, TDD cannot ensure the reliability of data. If you suffer any loss directly or indirectly because of the information on TDD, TDD will not responsible for that.",
+    "disclaimer_comment": "TDD keeps neutral to all the data. TDD does not and also not support any one-sided comments to anything (e.g. works, staffs, leagues, singers, etc.) only based on data. Data could be affected by lots of factors. Comments on anything should be considered comprehensively.",
+    "about_bunnyxt": "About bunnyxt",
+    "bunnyxt_intro": "VC fan since 2012. Fan of Tianyi Luo and Luo & Yuezheng CP. ",
+    "bunnyxt_social_media": "Follow me at sina weibo",
+    "bunnyxt_github": "INTJ, Github "
+  }
+}
+</i18n>
+
 <template>
   <div>
     <div class="tdd-breadcrumb">
       <a-breadcrumb>
-        <a-breadcrumb-item><router-link to="/">首页</router-link></a-breadcrumb-item>
-        <a-breadcrumb-item>关于</a-breadcrumb-item>
+        <a-breadcrumb-item><router-link to="/">{{ $t('page_name.home') }}</router-link></a-breadcrumb-item>
+        <a-breadcrumb-item>{{ $t('page_name.about') }}</a-breadcrumb-item>
       </a-breadcrumb>
     </div>
     <div class="section-block">
-      <h1>关于天钿Daily</h1>
+      <h1>{{ $t('about_tdd') }}</h1>
       <div style="float: right; margin-left: 8px; margin-bottom: 8px">
         <img :src="logo_max" alt="logo" width="64px" /><br>
         <span style="font-size: 10px">logo by <br>SweetFairyTale</span>
       </div>
-      <p>天钿Daily（TianDian Daily）是一个VC相关数据交流与分享的网站。本站致力于VC相关数据交流，基于B站API以及简单的网络爬虫，定期抓取VC相关数据，选取有意义的纬度展示。</p>
+      <p>{{ $t('tdd_intro') }}</p>
       <p>
-        QQ群：<a target="_blank" href="https://jq.qq.com/?_wv=1027&k=588s7nw">537793686</a><a-icon type="qrcode" style="margin-left: 8px; margin-right: 4px"/><a target="_blank" :href="qqgroup_qrcode">二维码</a><br/>
-        个人邮箱：<a href="mailto:bunnyxt@outlook.com">bunnyxt@outlook.com</a>
+        <i18n path="qq_group_info" tag="label">
+          <a target="_blank" href="https://jq.qq.com/?_wv=1027&k=588s7nw">537793686</a>
+          <a-icon type="qrcode" style="margin-left: 8px; margin-right: 4px"/>
+          <a target="_blank" :href="qqgroup_qrcode">{{ $t('qr_code') }}</a>
+        </i18n>
+        <br/>
+        <i18n path="personal_email_info" tag="label">
+          <a href="mailto:bunnyxt@outlook.com">bunnyxt@outlook.com</a>
+        </i18n>
       </p>
       <p>
-        <a href="https://afdian.net/@bunnyxt" target="_blank">资助本站</a> or <router-link to="/about/contactus">联系我们</router-link>
+        <a href="https://afdian.net/@bunnyxt" target="_blank">{{ $t('donate_tdd') }}</a> or <router-link to="/about/contactus">{{ $t('page_name.about_contactus') }}</router-link>
       </p>
     </div>
 <!--    <div class="section-separator"></div>-->
@@ -28,27 +91,45 @@
 <!--    </div>-->
     <div class="section-separator"></div>
     <div class="section-block">
-      <h1>技术相关</h1>
-      <p>小霸王服务器目前由阿里云与腾讯云提供，<strong>谢绝爆破</strong>。</p>
-      <p>前端使用Vue全家桶构建，包括vue-router前端路由以及vuex状态管理。界面使用<a href="https://ant.design" target="_blank">Ant Design</a> UI设计语言，并根据项目需要，针对移动端访问进行了一系列优化。图表采用Ant Vision <a href="https://g2.antv.vision/" target="_blank">G2</a>可视化引擎。</p>
-      <p>后端使用Spring Boot构建RESTful API，另外还有部分使用PHP实现的旧版接口。本站所有数据可以通过调用API获取。<strong>资源有限，请勿滥用。</strong>数据库依旧使用MySQL（因为不会用NoSQL）。</p>
-      <p>爬虫脚本基于Python。很丢人，目前还没使用多线程爬虫，不知道怎么避免触发412。</p>
-      <p>绝大部分源码可以在<a href="https://github.com/users/bunnyxt/projects/1" target="_blank">GitHub TianDian Daily Project</a>看到。点开右侧Menu，进入Linked repositories查看不同模块的项目。<strong>欢迎Star，欢迎Issue！</strong></p>
+      <h1>{{ $t('tech_related') }}</h1>
+      <p>
+        <i18n path="tech_server" tag="label">
+          <strong>{{ $t('tech_server_no_hack') }}</strong>
+        </i18n>
+      </p>
+      <p>
+        <i18n path="tech_frontend" tag="label">
+          <a href="https://ant.design" target="_blank">Ant Design</a>
+          <a href="https://g2.antv.vision/" target="_blank">G2</a>
+        </i18n>
+      </p>
+      <p>
+        <i18n path="tech_backend" tag="label">
+          <strong>{{ $t('tech_backend_api_limit') }}</strong>
+        </i18n>
+      </p>
+      <p>{{ $t('tech_crawler') }}</p>
+      <p>
+        <i18n path="tech_code" tag="label">
+          <a href="https://github.com/users/bunnyxt/projects/1" target="_blank">GitHub TianDian Daily Project</a>
+          <strong>{{ $t('tech_code_welcome') }}</strong>
+        </i18n>
+      </p>
     </div>
     <div class="section-separator"></div>
     <div class="section-block">
-      <h1>免责声明</h1>
-      <p>本站数据收集自BiliBili。</p>
-      <p>本站收集的所有数据均从公开渠道获取，不会收集任何敏感信息。</p>
-      <p>由于存在不可避免的网络问题或程序问题，本站无法保证所有数据的绝对可靠。若您因为本站的信息直接或间接蒙受任何损失，本站将不负任何责任。</p>
-      <p>本站对所有数据持中立态度，不发表也<strong>不鼓励</strong>基于数据的，对任何实体，包括作品、P主、社团、歌姬等的任何<strong>片面评价</strong>。数据受到多方面因素的影响，实体的评价也应该综合全面。</p>
+      <h1>{{ $t('disclaimer') }}</h1>
+      <p>{{ $t('disclaimer_source') }}</p>
+      <p>{{ $t('disclaimer_privacy') }}</p>
+      <p>{{ $t('disclaimer_reliability') }}</p>
+      <p>{{ $t('disclaimer_comment') }}</p>
     </div>
     <div class="section-separator"></div>
     <div class="section-block">
-      <h1>关于bunnyxt</h1>
-      <p>12年入坑的VC厨。洛厨，南北党。"<a href="https://www.bilibili.com/video/av456930/" target="_blank">默默守望，希望天依越来越好~</a>"</p>
-      <p>日常活跃于新浪微博 <a href="https://www.weibo.com/nny29" target="_blank">@牛奶源29</a></p>
-      <p>INTJ, naive coder from NJAU. 业余全栈工程师，GitHub <a href="https://github.com/bunnyxt" target="_blank">@bunnyxt</a></p>
+      <h1>{{ $t('about_bunnyxt') }}</h1>
+      <p>{{ $t('bunnyxt_intro') }}"<a href="https://www.bilibili.com/video/av456930/" target="_blank">默默守望，希望天依越来越好~</a>"</p>
+      <p>{{ $t('bunnyxt_social_media') }} <a href="https://www.weibo.com/nny29" target="_blank">@牛奶源29</a></p>
+      <p>{{ $t('bunnyxt_github') }} <a href="https://github.com/bunnyxt" target="_blank">@bunnyxt</a></p>
     </div>
   </div>
 </template>
--- src/components/page/about/ContactUs.vue
+++ src/components/page/about/ContactUs.vue
@@ -1,39 +1,86 @@
+<i18n src="@/i18n/common.json"></i18n>
+<i18n>
+{
+  "zh": {
+    "contact_official_info": "感谢您对天钿Daily的支持！天钿Daily目前由站长{0}一人开发、维护、运营，您可以通过以下方式与本站取得联系：",
+    "contact_official_qq_group": "官方QQ群：{0} {1} {2}",
+    "contact_official_webmaster_email": "站长个人邮箱：{0}",
+    "contact_official_admin_email": "本站管理员邮箱：{0}",
+    "contact_tech_info": "如果您对本站的技术实现感兴趣，可以关注以下链接：",
+    "contact_tech_github": "站长GitHub主页：{0}",
+    "contact_tech_github_project": "本项目GitHub Project主页：{0}",
+    "contact_personal_info": "站长个人活跃平台：",
+    "contact_personal_weibo": "新浪微博：{0}",
+    "contact_donate_info": "欢迎各位对本项目提出您的意见与建议，您的反馈对本站未来的发展非常重要！如果您喜欢本项目，也可以通过{0}资助本站。再次感谢您对天钿Daily的支持！希望这个小项目能对您有所帮助~"
+  },
+  "en": {
+    "contact_official_info": "Thanks for support TianDian Daily! TDD is currently developed, maintained, operated only by webmaster {0} himself. You can contact TDD via:",
+    "contact_official_qq_group": "Official QQ group: {0} {1} {2}",
+    "contact_official_webmaster_email": "Webmaster personal email: {0}",
+    "contact_official_admin_email": "TDD admin email: {0}",
+    "contact_tech_info": "If you are interested in the technology implement details, please visit:",
+    "contact_tech_github": "Webmaster's GitHub: {0}",
+    "contact_tech_github_project": "TDD GitHub Project: {0}",
+    "contact_personal_info": "Webmaster personal social media:",
+    "contact_personal_weibo": "Sina weibo: {0}",
+    "contact_donate_info": "If you have any questions or suggestions, feel free to contact us! Your feedbacks are really important to us. If you like this project, you can donate TDD via {0}. Thanks to your support again! Hope this project can help you."
+  }
+}
+</i18n>
+
 <template>
   <div>
     <div class="tdd-breadcrumb">
       <a-breadcrumb>
-        <a-breadcrumb-item><router-link to="/">首页</router-link></a-breadcrumb-item>
-        <a-breadcrumb-item><router-link to="/about">关于</router-link></a-breadcrumb-item>
-        <a-breadcrumb-item>联系我们</a-breadcrumb-item>
+        <a-breadcrumb-item><router-link to="/">{{ $t('page_name.home') }}</router-link></a-breadcrumb-item>
+        <a-breadcrumb-item><router-link to="/about">{{ $t('page_name.about') }}</router-link></a-breadcrumb-item>
+        <a-breadcrumb-item>{{ $t('page_name.about_contactus') }}</a-breadcrumb-item>
       </a-breadcrumb>
     </div>
     <div class="section-block">
-      <h1>联系我们</h1>
-      <p>感谢您对天钿Daily的支持！天钿Daily目前由站长<a href="https://www.bunnyxt.com" target="_blank">bunnyxt</a>一人开发、维护、运营，您可以通过以下方式与本站取得联系：</p>
+      <h1>{{ $t('page_name.about_contactus') }}</h1>
+      <p>
+        <i18n path="contact_official_info" tag="label">
+          <a href="https://www.bunnyxt.com" target="_blank">bunnyxt</a>
+        </i18n>
       <ul>
         <li>
-          官方QQ群：<a target="_blank" href="https://jq.qq.com/?_wv=1027&k=588s7nw">537793686</a><a-icon type="qrcode" style="margin-left: 8px; margin-right: 4px"/><a target="_blank" :href="qqgroup_qrcode">二维码</a>
+          <i18n path="contact_official_qq_group" tag="label">
+            <a target="_blank" href="https://jq.qq.com/?_wv=1027&k=588s7nw">537793686</a>
+            <a-icon type="qrcode" style="margin-left: 8px; margin-right: 4px"/>
+            <a target="_blank" :href="qqgroup_qrcode">{{ $t('qr_code') }}</a>
+          </i18n>
         </li>
         <li>
-          站长个人邮箱：<a href="mailto:bunnyxt@outlook.com">bunnyxt@outlook.com</a>
+          <i18n path="contact_official_webmaster_email" tag="label">
+            <a href="mailto:bunnyxt@outlook.com">bunnyxt@outlook.com</a>
+          </i18n>
         </li>
         <li>
-          本站管理员邮箱：<a href="mailto:admin@bunnyxt.com">admin@bunnyxt.com</a>
+          <i18n path="contact_official_admin_email" tag="label">
+            <a href="mailto:admin@bunnyxt.com">admin@bunnyxt.com</a>
+          </i18n>
         </li>
       </ul>
-      <p>如果您对本站的技术实现感兴趣，可以关注以下链接：</p>
+      <p>{{ $t('contact_tech_info') }}</p>
       <ul>
         <li>
-          站长GitHub主页：<a target="_blank" href="https://github.com/bunnyxt">https://github.com/bunnyxt</a>
+          <i18n path="contact_tech_github" tag="label">
+            <a target="_blank" href="https://github.com/bunnyxt">https://github.com/bunnyxt</a>
+          </i18n>
         </li>
         <li>
-          本项目GitHub Project主页：<a target="_blank" href="https://github.com/users/bunnyxt/projects/1">https://github.com/users/bunnyxt/projects/1</a>
+          <i18n path="contact_tech_github_project" tag="label">
+            <a target="_blank" href="https://github.com/users/bunnyxt/projects/1">https://github.com/users/bunnyxt/projects/1</a>
+          </i18n>
         </li>
       </ul>
-      <p>站长个人活跃平台：</p>
+      <p>{{ $t('contact_personal_info') }}</p>
       <ul>
         <li>
-          新浪微博：<a target="_blank" href="https://weibo.com/nny29">@牛奶源29</a>
+          <i18n path="contact_personal_weibo" tag="label">
+            <a target="_blank" href="https://weibo.com/nny29">@牛奶源29</a>
+          </i18n>
         </li>
         <li>
           BiliBili：<a target="_blank" href="https://space.bilibili.com/1635775">@牛奶源</a>
@@ -42,7 +89,11 @@
           Twitter：<a target="_blank" href="https://twitter.com/bunnyxt29">@bunnyxt29</a>
         </li>
       </ul>
-      <p>欢迎各位对本项目提出您的意见与建议，您的反馈对本站未来的发展非常重要！如果您喜欢本项目，也可以通过<a href="https://afdian.net/@bunnyxt" target="_blank">爱发电</a>资助本站。再次感谢您对天钿Daily的支持！希望这个小项目能对您有所帮助~</p>
+      <p>
+        <i18n path="contact_donate_info" tag="label">
+          <a href="https://afdian.net/@bunnyxt" target="_blank">{{ $t('afdian') }}</a>
+        </i18n>
+      </p>
     </div>
   </div>
 </template>
