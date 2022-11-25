06e17144034626ff4da88e0867b3ed490a51f456
bunnyxt <bunnyxt@outlook.com>
2022-11-24 23:01:44 -0800

    chore: support `i18n` block in `.vue` file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueI18n({
+      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+      compositionOnly: false,
+
+      // you need to set i18n resource including paths !
+      include: path.resolve(__dirname, './src/i18n/**'),
+    }),
+  ],
   resolve: {
     // TODO: remove 'extensions', add .vue for each component import
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
